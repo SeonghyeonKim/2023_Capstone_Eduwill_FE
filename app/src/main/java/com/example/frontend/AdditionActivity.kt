b0947501a1fb2867fcefc51e2278db0bae7900b6
SeonghyeonKim <kkgojn@naver.com>
2023-09-12 16:48:57 +0900

    Test

--- app/src/main/java/com/example/frontend/AdditionActivity.kt
+++ app/src/main/java/com/example/frontend/AdditionActivity.kt
@@ -1,8 +1,13 @@
 package com.example.frontend
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
 import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.frontend.databinding.ActivityAdditionBinding
 
@@ -18,14 +23,56 @@ class AdditionActivity: AppCompatActivity() {
         binding = ActivityAdditionBinding.inflate(layoutInflater)
 
         initHttpRequest()
+        binding.addUpdateBtn.setOnClickListener {
+            Toast.makeText(this, "테스트", Toast.LENGTH_SHORT).show()
+            httpRequestUpload()
+        }
 
-        setContentView(R.layout.activity_addition)
+        setContentView(binding.root)
     }
-
 
     fun initHttpRequest() {
-        MainActivity.requestQueue = Volley.newRequestQueue(this)
+        requestQueue = Volley.newRequestQueue(this)
     }
 
+    fun httpRequestUpload() {
+        val request = object: StringRequest(
+            Request.Method.POST,
+            "http://172.22.54.97:3389/upload",
+            Response.Listener<String> {
+                finish()
+            },
+            Response.ErrorListener {
+                Toast.makeText(this, "업로드에 실패하였습니다.", Toast.LENGTH_LONG).show()
+            }
+        ) {
+            override fun getParams(): MutableMap<String, String> {
+                val params: MutableMap<String, String> = HashMap()
+                params["userId"] = "10"
+                params["title"] = binding.addName.text.toString()
+                params["content"] = binding.addInform.text.toString()
+                params["price"] = binding.addPrice.text.toString()
+                params["numberOfImages"] = "0"
+                params["video"] = ""
+                params["image_1"] = ""
+                params["image_2"] = ""
+                params["image_3"] = ""
+                params["image_4"] = ""
+                params["image_5"] = ""
+                params["xSize"] = binding.addXSize.text.toString()
+                params["ySize"] = binding.addYSize.text.toString()
+                params["zSize"] = binding.addZSize.text.toString()
 
+                return params
+            }
+
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String,String>()
+                headers.put("Content-Type","multipart/form-data")
+                return headers
+            }
+        }
+        request.setShouldCache(false)
+        requestQueue?.add(request)
+    }
 }
